a2149f6b58f2fb2ad7d62d9422329cf950c96b8d
BadAcronym <terribleacronym@gmail.com>
2026-02-02 10:29:39 +0100

    expand testing with messages

--- src/imgsurf_loader_main.c
+++ src/imgsurf_loader_main.c
@@ -1,8 +1,9 @@
 #include "imgsurf_loader.h"
 
-internal void loadPNG
+internal uint8_t* loadPNG
 (
 ){
+    return 0;
 }
 
 internal void findFormat
@@ -26,6 +27,7 @@ internal void findFormat
     if(period == 0)
     {
         fprintf(stderr, "Path doesn't look like a file. Please check or add a file extension (.png/.bmp...)\n");
+        return;
     }
 
     //NOTE: should prevent accessing beyond string, no supported extension name is < 2 || > 4
@@ -34,29 +36,29 @@ internal void findFormat
         return;
     }
 
-    if(path[period] == 'p' && path[period + 1] == 'n' && path[period + 2] == 'g')
+    if(path[period + 1] == 'p' && path[period + 2] == 'n' && path[period + 3] == 'g')
     {
         *format = IMGSURF_FILE_PNG;
     }
-    else if(path[period] == 'b' && path[period + 1] == 'm' && path[period + 2] == 'p')
+    else if(path[period + 1] == 'b' && path[period + 2] == 'm' && path[period + 3] == 'p')
     {
         *format = IMGSURF_FILE_BMP;
     }
-    else if(path[period] == 'w' && path[period + 1] == 'e' &&
-            path[period + 2] == 'b' && path[period + 3] == 'p')
+    else if(path[period + 1] == 'w' && path[period + 2] == 'e' &&
+            path[period + 3] == 'b' && path[period + 4] == 'p')
     {
         *format = IMGSURF_FILE_WEBP;
     }
-    else if(path[period] == 'a' && path[period + 1] == 'v' &&
-            path[period + 2] == 'i' && path[period + 3] == 'f')
+    else if(path[period + 1] == 'a' && path[period + 2] == 'v' &&
+            path[period + 3] == 'i' && path[period + 4] == 'f')
     {
         *format = IMGSURF_FILE_AVIF;
     }
-    else if(path[period] == 'q' && path[period + 1] == 'o' && path[period + 2] == 'i')
+    else if(path[period + 1] == 'q' && path[period + 2] == 'o' && path[period + 3] == 'i')
     {
         *format = IMGSURF_FILE_QOI;
     }
-    else if(path[period] == 'j' && path[period + 1] == 'x' && path[period + 2] == 'l')
+    else if(path[period + 1] == 'j' && path[period + 2] == 'x' && path[period + 3] == 'l')
     {
         *format = IMGSURF_FILE_JXL;
     }
@@ -68,30 +70,75 @@ uint8_t* imgsurf_load
     uint8_t     channels,
     uint8_t     bitdepth
 ){
+    uint8_t* image = 0;
+
     uint8_t format = UINT8_MAX;
     findFormat(path, &format);
 
     if(format == UINT8_MAX)
     {
         fprintf(stderr, "File format is not supported. Try a .png/.bmp/.webp/.avif/.qoi/.jxl file.\n");
+        return 0;
     }
 
     if(channels > IMGSURF_CHANNELS_MAX)
     {
         fprintf(stderr, "Invalid colour channels specified.\n");
+        return 0;
     }
 
     if(bitdepth == 0)
     {
         fprintf(stderr, "Bit depth cannot be null.\n");
+        return 0;
     }
 
-    if(format == IMGSURF_FILE_PNG && bitdepth > 2 && bitdepth != 4 && bitdepth != 8 && bitdepth != 16)
+    //TODO: more bit depth checking? maybe less strict, if it can load with wrong depth, let it
+    switch(format)
     {
-        fprintf(stderr, "Only bit depths of 1, 2, 4, 8 or 16 are supported by .png!\n");
+        case IMGSURF_FILE_PNG:
+        {
+            if(bitdepth > 2 && bitdepth != 4 && bitdepth != 8 && bitdepth != 16)
+            {
+                fprintf(stderr, "Only bit depths of 1, 2, 4, 8 or 16 are supported by .png!\n");
+            }
+            else
+            {
+                image = loadPNG();
+            }
+            break;
+        }
+        case IMGSURF_FILE_BMP:
+        {
+            fprintf(stderr, "Format BMP not supported yet.\n");
+            break;
+        }
+        case IMGSURF_FILE_WEBP:
+        {
+            fprintf(stderr, "Format WEBP not supported yet.\n");
+            break;
+        }
+        case IMGSURF_FILE_AVIF:
+        {
+            fprintf(stderr, "Format AVIF not supported yet.\n");
+            break;
+        }
+        case IMGSURF_FILE_QOI:
+        {
+            fprintf(stderr, "Format QOI not supported yet.\n");
+            break;
+        }
+        case IMGSURF_FILE_JXL:
+        {
+            fprintf(stderr, "Format JXL not supported yet.\n");
+            break;
+        }
+        default:
+        {
+            fprintf(stderr, "Format not supported, code path should be impossible.\n");
+            return 0;
+        }
     }
 
-    //delegate loading based on parsed format, after parameter checking
-
-    return 0;
+    return image;
 }
--- src/imgsurftest_main.c
+++ src/imgsurftest_main.c
@@ -2,5 +2,26 @@
 
 int main()
 {
-    imgsurf_load("assets/test.png", IMGSURF_CHANNELS_RGBA, 8);
+    uint8_t* png = imgsurf_load("assets/test.png", IMGSURF_CHANNELS_RGBA, 8);
+
+    if(!png)
+    {
+        fprintf(stderr, "imgsurf_load failed to load a png.\n");
+    }
+
+    printf("R: %u", png[0]);
+    printf("G: %u", png[0]);
+    printf("B: %u", png[0]);
+    printf("A: %u", png[0]);
+
+    uint8_t* bmp = imgsurf_load("assets/test.bmp", IMGSURF_CHANNELS_RGB, 8);
+
+    if(!bmp)
+    {
+        fprintf(stderr, "imgsurf_load failed to load a bmp.\n");
+    }
+
+    printf("R: %u", bmp[0]);
+    printf("G: %u", bmp[0]);
+    printf("B: %u", bmp[0]);
 }
