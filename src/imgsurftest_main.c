85a431f32c9c4248e45961bf0bd8e393d041ee7c
BadAcronym <terribleacronym@gmail.com>
2026-02-02 10:29:39 +0100

    expand tests

--- src/imgsurftest_main.c
+++ src/imgsurftest_main.c
@@ -14,11 +14,11 @@ int main()
     }
     else
     {
-        printf("PNG loaded!\n");
-        printf("R: %u", png[0]);
-        printf("G: %u", png[0]);
-        printf("B: %u", png[0]);
-        printf("A: %u", png[0]);
+        printf("\nPNG loaded!\n");
+        printf("R:%u ", png[0]);
+        printf("G:%u ", png[1]);
+        printf("B:%u ", png[2]);
+        printf("A:%u\n", png[3]);
     }
 
     uint8_t* bmp = imgsurf_load("assets/tux.bmp", &width, &height, IMGSURF_CHANNELS_RGB, 8);
@@ -28,10 +28,10 @@ int main()
     }
     else
     {
-        printf("BMP loaded!\n");
-        printf("R: %u", bmp[0]);
-        printf("G: %u", bmp[0]);
-        printf("B: %u", bmp[0]);
+        printf("\nBMP loaded!\n");
+        printf("R:%u ", bmp[0]);
+        printf("G:%u ", bmp[1]);
+        printf("B:%u\n", bmp[2]);
     }
 
     uint8_t* qoiRGBA = imgsurf_load("assets/tux.qoi", &width, &height, IMGSURF_CHANNELS_RGBA, 8);
@@ -41,10 +41,11 @@ int main()
     }
     else
     {
-        printf("QOI loaded!\n");
-        printf("R: %u", qoiRGBA[0]);
-        printf("G: %u", qoiRGBA[0]);
-        printf("B: %u", qoiRGBA[0]);
+        printf("\nQOI in RGBA loaded!\n");
+        printf("R:%u ", qoiRGBA[0]);
+        printf("G:%u ", qoiRGBA[1]);
+        printf("B:%u ", qoiRGBA[2]);
+        printf("A:%u\n", qoiRGBA[3]);
     }
 
     uint8_t* qoiBGRA = imgsurf_load("assets/tux.qoi", &width, &height, IMGSURF_CHANNELS_BGRA, 8);
@@ -54,9 +55,36 @@ int main()
     }
     else
     {
-        printf("QOI loaded!\n");
-        printf("R: %u", qoiBGRA[0]);
-        printf("G: %u", qoiBGRA[0]);
-        printf("B: %u", qoiBGRA[0]);
+        printf("\nQOI in BGRA loaded!\n");
+        printf("B:%u ", qoiBGRA[0]);
+        printf("G:%u ", qoiBGRA[1]);
+        printf("R:%u ", qoiBGRA[2]);
+        printf("A:%u\n", qoiBGRA[3]);
+    }
+
+    uint8_t* qoiRGB = imgsurf_load("assets/tux.qoi", &width, &height, IMGSURF_CHANNELS_RGB, 8);
+    if(!qoiRGB)
+    {
+        fprintf(stderr, "imgsurf_load failed to load a qoi in RGBA.\n");
+    }
+    else
+    {
+        printf("\nQOI in RGB loaded!\n");
+        printf("R:%u ", qoiBGRA[0]);
+        printf("G:%u ", qoiBGRA[1]);
+        printf("B:%u\n", qoiBGRA[2]);
+    }
+
+    uint8_t* qoiBGR = imgsurf_load("assets/tux.qoi", &width, &height, IMGSURF_CHANNELS_BGR, 8);
+    if(!qoiBGR)
+    {
+        fprintf(stderr, "imgsurf_load failed to load a qoi in RGBA.\n");
+    }
+    else
+    {
+        printf("\nQOI in BGR loaded!\n");
+        printf("B:%u ", qoiBGRA[0]);
+        printf("G:%u ", qoiBGRA[1]);
+        printf("R:%u\n", qoiBGRA[2]);
     }
 }
