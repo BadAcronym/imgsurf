662ec16f817d714f1b6edea49650ba2c81cbf0ab
BadAcronym <terribleacronym@gmail.com>
2026-02-02 10:29:40 +0100

    add stat variation to windows builds

---
+++ src/win32_imgsurf_platform.c
@@ -0,0 +1,28 @@
+#include "imgsurf_load.h"
+#include "linux_imgsurf_platform.h"
+
+#include <sys/stat.h>
+
+uint8_t imgsurf_verifyPath
+(
+    const char *path
+){
+    struct _stat pathInfo;
+
+    if(_stat(path, &pathInfo))
+    {
+        return IMGSURF_TYPE_ERROR;
+    }
+
+    if(S_ISDIR(pathInfo.st_mode))
+    {
+        return IMGSURF_TYPE_DIRECTORY;
+    }
+
+    if(S_ISREG(pathInfo.st_mode))
+    {
+        return IMGSURF_TYPE_FILE;
+    }
+
+    return IMGSURF_TYPE_OTHER;
+}
