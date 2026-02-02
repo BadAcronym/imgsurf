5c14b3ce89bea955184230ea904c8a294a5ed31f
BadAcronym <terribleacronym@gmail.com>
2026-02-02 10:29:39 +0100

    basic function layout

--- include/imgsurf_loader.h
+++ include/imgsurf_loader.h
@@ -1,1 +1,26 @@
+#pragma once
 
+#include <stdint.h>
+#include <stdio.h>
+
+#define persistent  static
+#define global      static
+#define internal    static
+
+#define IMGSURF_CHANNELS_MIN  0
+#define IMGSURF_CHANNELS_RGBA 0
+#define IMGSURF_CHANNELS_RGB  1
+#define IMGSURF_CHANNELS_MAX  1
+
+#define IMGSURF_FILE_MIN  10
+#define IMGSURF_FILE_PNG  10
+#define IMGSURF_FILE_BMP  11
+#define IMGSURF_FILE_MAX  11
+
+//TODO: provide some way to flip channels around in specified format
+extern uint8_t* imgsurf_load
+(
+    const char* path,
+    uint8_t     format,
+    uint8_t     bitdepth
+);
--- src/imgsurf_loader_main.c
+++ src/imgsurf_loader_main.c
@@ -1,3 +1,50 @@
+#include "imgsurf_loader.h"
+
 int main()
 {
+}
+
+internal void loadPNG
+(
+){
+}
+
+internal void findFormat
+(
+    const char* path,
+    uint8_t     *format
+){
+    //write back found format
+}
+
+uint8_t* imgsurf_load
+(
+    const char* path,
+    uint8_t     channels,
+    uint8_t     bitdepth
+){
+    uint8_t format = 0;
+    findFormat(path, &format);
+
+    if(!format)
+    {
+        fprintf(stderr, "File format is not supported. Try a .png/.bmp/.webp/.avif/.qoi/.jxl file.\n");
+    }
+
+    if(channels < IMGSURF_CHANNELS_MIN || channels > IMGSURF_CHANNELS_MIN)
+    {
+        fprintf(stderr, "Invalid colour channels specified.\n");
+    }
+
+    if(bitdepth == 0)
+    {
+        fprintf(stderr, "Bit depth cannot be null.\n");
+    }
+
+    if(format == IMGSURF_FILE_PNG && bitdepth > 2 && bitdepth != 4 && bitdepth != 8 && bitdepth != 16)
+    {
+        fprintf(stderr, "Only bit depths of 1, 2, 4, 8 or 16 are supported by .png!\n");
+    }
+
+    //delegate loading based on parsed format
 }
