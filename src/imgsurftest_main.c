ccb1d18c08cad2997f10914173e21a69115402b9
BadAcronym <terribleacronym@gmail.com>
2026-09-06 16:08:21 +0200

    properly set up writing tests for later

--- src/imgsurftest_main.c
+++ src/imgsurftest_main.c
@@ -15,14 +15,14 @@ f_internal bool verifyImage_read
     uint8_t *testQOI = imLoadFile(name_qoi, &width, &height, IM_CHANNELS_RGBA, 8);
     if(!testQOI)
     {
-        fprintf(stderr, "\x1b[1;31m\nimLoadFile failed on QOI.\033[0m\n");
+        fprintf(stderr, "\033[1;31m\nimLoadFile failed on QOI.\033[0m\n");
         return false;
     }
 
     uint8_t *testPNG = imLoadFile(name_png, &width, &height, IM_CHANNELS_RGBA, 8);
     if(!testPNG)
     {
-        fprintf(stderr, "\x1b[1;31m\nimLoadFile failed on PNG.\033[0m\n");
+        fprintf(stderr, "\033[1;31m\nimLoadFile failed on PNG.\033[0m\n");
         free(testQOI);
         return false;
     }
@@ -31,7 +31,7 @@ f_internal bool verifyImage_read
     {
         if(testQOI[i] != testPNG[i])
         {
-            fprintf(stderr, "\x1b[1;31mimLoadFile failed @ pixel %lu.red, expected: "
+            fprintf(stderr, "\033[1;31mimLoadFile failed @ pixel %lu.red, expected: "
                     "%u, got: %u\n", i / 4, testQOI[i], testPNG[i]);
             free(testQOI);
             free(testPNG);
@@ -39,7 +39,7 @@ f_internal bool verifyImage_read
         }
         if(testQOI[i + 1] != testPNG[i + 1])
         {
-            fprintf(stderr, "\x1b[1;31mimLoadFile failed @ pixel %lu.green, expected: "
+            fprintf(stderr, "\033[1;31mimLoadFile failed @ pixel %lu.green, expected: "
                     "%u, got: %u\n", i / 4, testQOI[i + 1], testPNG[i + 1]);
             free(testQOI);
             free(testPNG);
@@ -47,7 +47,7 @@ f_internal bool verifyImage_read
         }
         if(testQOI[i + 2] != testPNG[i + 2])
         {
-            fprintf(stderr, "\x1b[1;31mimLoadFile failed @ pixel %lu.blue, expected: "
+            fprintf(stderr, "\033[1;31mimLoadFile failed @ pixel %lu.blue, expected: "
                     "%u, got: %u\n", i / 4, testQOI[i + 2], testPNG[i + 2]);
             free(testQOI);
             free(testPNG);
@@ -55,7 +55,7 @@ f_internal bool verifyImage_read
         }
         if(testQOI[i + 3] != testPNG[i + 3])
         {
-            fprintf(stderr, "\x1b[1;31mimLoadFile failed @ pixel %lu.alpha, expected:"
+            fprintf(stderr, "\033[1;31mimLoadFile failed @ pixel %lu.alpha, expected:"
                     " %u, got: %u\n", i / 4, testQOI[i + 3], testPNG[i + 3]);
             free(testQOI);
             free(testPNG);
@@ -70,72 +70,129 @@ f_internal bool verifyImage_read
 
 f_internal bool verifyImage_write
 (
-    const char *name_qoi,
-    const char *name_png
+    const char *name
 ){
-    fprintf(stderr, "\ntrying to verify writing image: %s with %s\n",
-            name_qoi, name_png);
+    fprintf(stderr, "\ntrying to verify writing loaded image: '%s'\n", name);
 
     uint32_t width  = 0;
     uint32_t height = 0;
 
-    uint8_t *testPNG = imLoadFile(name_png, &width, &height, IM_CHANNELS_RGBA, 8);
-    if(!testPNG)
+    uint8_t *test = imLoadFile(name, &width, &height, IM_CHANNELS_RGBA, 8);
+    if(!test)
     {
-        fprintf(stderr, "\x1b[1;31mimLoadFile failed on PNG.\033[0m\n");
+        fprintf(stderr, "\033[1;31mimLoadFile failed on QOI.\033[0m\n");
         return false;
     }
 
-    if(!imWriteFile("assets/reconstructed.qoi", testPNG, width, height,
+    if(!imWriteFile("assets/reconstructed.qoi", test, width, height,
                     IM_CHANNELS_RGBA, 8, IM_FILE_QOI)
     ){
-        fprintf(stderr, "\x1b[1;31mimWriteFile failed on QOI.\033[0m\n");
-        free(testPNG);
+        fprintf(stderr, "\033[1;31mimWriteFile failed on QOI.\033[0m\n");
+        free(test);
         return false;
     }
 
     uint8_t *reconstructedQOI = imLoadFile("assets/reconstructed.qoi", &width, &height,
                                            IM_CHANNELS_RGBA, 8);
+    if(!reconstructedQOI)
+    {
+        fprintf(stderr, "\033[1;31mimLoadFile failed to load reconstructed QOI image."
+                "\033[0m\n");
+        return false;
+    }
 
     for(uint64_t i = 0; i < width * height * 4; i += 4)
     {
-        if(reconstructedQOI[i] != testPNG[i])
+        if(reconstructedQOI[i] != test[i])
         {
-            fprintf(stderr, "\x1b[1;31mimWriteFile failed @ pixel %lu.red, expected: "
-                    "%u, got: %u\n", i / 4, reconstructedQOI[i], testPNG[i]);
-            free(testPNG);
+            fprintf(stderr, "\033[1;31mimWriteFile failed @ pixel %lu.red, expected: "
+                    "%u, got: %u\n", i / 4, reconstructedQOI[i], test[i]);
+            free(test);
             free(reconstructedQOI);
             return false;
         }
-        if(reconstructedQOI[i + 1] != testPNG[i + 1])
+        if(reconstructedQOI[i + 1] != test[i + 1])
         {
-            fprintf(stderr, "\x1b[1;31mimWriteFile failed @ pixel %lu.green, "
-                    "expected: %u, got: %u\n", i / 4, reconstructedQOI[i], testPNG[i]);
-            free(testPNG);
+            fprintf(stderr, "\033[1;31mimWriteFile failed @ pixel %lu.green, expected: "
+                    "%u, got: %u\n", i / 4, reconstructedQOI[i + 1], test[i + 1]);
+            free(test);
             free(reconstructedQOI);
             return false;
         }
-        if(reconstructedQOI[i + 2] != testPNG[i + 2])
+        if(reconstructedQOI[i + 2] != test[i + 2])
         {
-            fprintf(stderr, "\x1b[1;31mimWriteFile failed @ pixel %lu.blue, expected:"
-                    " %u, got: %u\n", i / 4, reconstructedQOI[i], testPNG[i]);
-            free(testPNG);
+            fprintf(stderr, "\033[1;31mimWriteFile failed @ pixel %lu.blue, expected: "
+                    "%u, got: %u\n", i / 4, reconstructedQOI[i + 2], test[i + 2]);
+            free(test);
             free(reconstructedQOI);
             return false;
         }
-        if(reconstructedQOI[i + 3] != testPNG[i + 3])
+        if(reconstructedQOI[i + 3] != test[i + 3])
         {
-            fprintf(stderr, "\x1b[1;31mimWriteFile failed @ pixel %lu.alpha, "
-                    "expected: %u, got: %u\n", i / 4, reconstructedQOI[i], testPNG[i]);
-            free(testPNG);
+            fprintf(stderr, "\033[1;31mimWriteFile failed @ pixel %lu.alpha, expected"
+                    ": %u, got: %u\n", i / 4, reconstructedQOI[i + 3], test[i + 3]);
+            free(test);
             free(reconstructedQOI);
             return false;
         }
     }
 
-    free(testPNG);
     free(reconstructedQOI);
 
+    if(!imWriteFile("assets/reconstructed.png", test, width, height,
+                    IM_CHANNELS_RGBA, 8, IM_FILE_PNG)
+    ){
+        fprintf(stderr, "\033[1;31mimWriteFile failed on PNG.\033[0m\n");
+        free(test);
+        return false;
+    }
+
+    uint8_t *reconstructedPNG = imLoadFile("assets/reconstructed.png", &width, &height,
+                                           IM_CHANNELS_RGBA, 8);
+    if(!reconstructedPNG)
+    {
+        fprintf(stderr, "\033[1;31mimLoadFile failed to load reconstructed PNG image."
+                "\033[0m\n");
+        return false;
+    }
+
+    for(uint64_t i = 0; i < width * height * 4; i += 4)
+    {
+        if(reconstructedPNG[i] != test[i])
+        {
+            fprintf(stderr, "\033[1;31mimWriteFile failed @ pixel %lu.red, expected: "
+                    "%u, got: %u\n", i / 4, reconstructedPNG[i], test[i]);
+            free(test);
+            free(reconstructedPNG);
+            return false;
+        }
+        if(reconstructedPNG[i + 1] != test[i + 1])
+        {
+            fprintf(stderr, "\033[1;31mimWriteFile failed @ pixel %lu.green, expected: "
+                    "%u, got: %u\n", i / 4, reconstructedPNG[i + 1], test[i + 1]);
+            free(test);
+            free(reconstructedPNG);
+            return false;
+        }
+        if(reconstructedPNG[i + 2] != test[i + 2])
+        {
+            fprintf(stderr, "\033[1;31mimWriteFile failed @ pixel %lu.blue, expected: "
+                    "%u, got: %u\n", i / 4, reconstructedPNG[i + 2], test[i + 2]);
+            free(test);
+            free(reconstructedPNG);
+            return false;
+        }
+        if(reconstructedPNG[i + 3] != test[i + 3])
+        {
+            fprintf(stderr, "\033[1;31mimWriteFile failed @ pixel %lu.alpha, expected"
+                    ": %u, got: %u\n", i / 4, reconstructedPNG[i + 3], test[i + 3]);
+            free(test);
+            free(reconstructedPNG);
+            return false;
+        }
+    }
+
+    free(test);
     return true;
 }
 
@@ -147,7 +204,7 @@ int main
 
     if(!verifyImage_read("assets/smallTest.qoi", "assets/smallTest.png"))
     {
-        fprintf(stderr, "\x1b[7;31mERROR: READ test not passed with image "
+        fprintf(stderr, "\033[7;31mERROR: READ test not passed with image "
                 "assets/smallTest!\033[0m\n");
         result = false;
     }
@@ -159,7 +216,7 @@ int main
 
     if(!verifyImage_read("assets/black.qoi", "assets/black.png"))
     {
-        fprintf(stderr, "\x1b[7;31mERROR: READ test not passed with image "
+        fprintf(stderr, "\033[7;31mERROR: READ test not passed with image "
                 "assets/black!\033[0m\n");
         result = false;
     }
@@ -171,7 +228,7 @@ int main
 
     if(!verifyImage_read("assets/tux.qoi", "assets/tux.png"))
     {
-        fprintf(stderr, "\x1b[7;31mERROR: READ test not passed with image "
+        fprintf(stderr, "\033[7;31mERROR: READ test not passed with image "
                 "assets/tux!\033[0m\n");
         result = false;
     }
@@ -181,9 +238,9 @@ int main
                 "assets/tux!\033[0m\n");
     }
 
-    if(!verifyImage_write("assets/smallTest.qoi", "assets/smallTest.png"))
+    if(!verifyImage_write("assets/smallTest.qoi"))
     {
-        fprintf(stderr, "\x1b[7;31mERROR: WRITE test not passed with image "
+        fprintf(stderr, "\033[7;31mERROR: WRITE test not passed with image "
                 "assets/smallTest!\033[0m\n");
         result = false;
     }
@@ -193,9 +250,9 @@ int main
                 "assets/smallTest!\033[0m\n");
     }
 
-    if(!verifyImage_write("assets/black.qoi", "assets/black.png"))
+    if(!verifyImage_write("assets/black.qoi"))
     {
-        fprintf(stderr, "\x1b[7;31mERROR: WRITE test not passed with image "
+        fprintf(stderr, "\033[7;31mERROR: WRITE test not passed with image "
                 "assets/black!\033[0m\n");
         result = false;
     }
@@ -205,9 +262,9 @@ int main
                 "assets/black!\033[0m\n");
     }
 
-    if(!verifyImage_write("assets/tux.qoi", "assets/tux.png"))
+    if(!verifyImage_write("assets/tux.qoi"))
     {
-        fprintf(stderr, "\x1b[7;31mERROR: WRITE test not passed with image "
+        fprintf(stderr, "\033[7;31mERROR: WRITE test not passed with image "
                 "assets/tux!\033[0m\n");
         result = false;
     }
