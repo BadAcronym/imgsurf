3a62de81f51da0070ccd83f4ef147c6e0d46e5c6
badacronym <terribleacronym@gmail.com>
2026-09-07 15:10:11 +0200

    fixup win32 run.ps1

--- run.ps1
+++ run.ps1
@@ -4,17 +4,7 @@ param
     [Parameter(Position = 1)][string]$compile_only
 )
 
-if(-Not(Test-Path "./obj/"))
-{
-    mkdir "./obj/"
-}
-
-if(-Not(Test-Path "./build/"))
-{
-    mkdir "./build/"
-}
-
-if(-Not(Test-Path "./bin/"))
+if(-Not(Test-Path "./bin/" -PathType Container))
 {
     mkdir "./bin/"
 }
@@ -24,32 +14,111 @@ if($build -eq $null -or $build -eq "")
     $build = "release"
 }
 
-if($build -eq "asan" -or $build -eq "debug" -or $build -eq "release")
-{
-    Write-Host "`ncompiling imgsurf...`n" -Fore Cyan
+$args_always=@("-DBUILD_WINDOWS",
+"src/imgsurf_format_bmp.c",
+"src/imgsurf_format_qoi.c",
+"src/imgsurf_format_png.c",
+"src/imgsurf_main.c",
+"src/imgsurftest_main.c",
+"vendor/datasurf/src/datasurf_formats.c",
+"vendor/datasurf/src/datasurf_algo_deflate.c",
+"vendor/datasurf/vendor/puddle/src/win32_pd_path.c",
+"vendor/datasurf/vendor/puddle/src/string_view.c",
+"-Iinclude",
+"-Ivendor/datasurf/include",
+"-Ivendor/datasurf/vendor/puddle/include",
+"-std=c99",
+"-Wextra", "-Wall", "-Wpedantic", "-Wconversion", "-Wshadow", "-Wsign-compare",
+"-Wtype-limits", "-Wunused",
+"-Wno-unsafe-buffer-usage", "-Wno-declaration-after-statement", "-Wno-vla",
+"-Wno-implicit-void-ptr-cast")
 
-    premake5 gmake
-    pushd "./build/"
-    make config=$build`_windows
-    popd
+$args_release=@("-O2")
+
+$args_debug=@("-DDEBUG", "-gcodeview", "-O0")
+$args_debug_cl=@("/DDEBUG", "/Zi", "/Od")
+
+$args_asan=$args_debug_cl+@("-oa.exe", "/clang:-std=c99", "/DASAN",
+"/fsanitize=address", "/MD",
+"/link", "/SUBSYSTEM:CONSOLE")
+
+function compile
+{
+    param( [string[]]$1 )
+
+    Write-Host "identifying a compiler..."
+
+    if($build -eq "asan")
+    {
+        if(-Not(Get-Command clang -ErrorAction SilentlyContinue))
+        {
+            Write-Host "ERROR: clang-cl needed for address sanitization." -Fore Red
+        }
+        $script:compiler="clang-cl"
+    }
+    elseif(Get-Command clang -ErrorAction SilentlyContinue)
+    {
+        Write-Host "found clang."
+        $script:compiler="clang"
+    }
+    elseif(Get-Command gcc -ErrorAction SilentlyContinue)
+    {
+        Write-Host "found gcc."
+        $script:compiler="gcc"
+    }
+    else
+    {
+        Write-Host "ERROR: no suitable compiler found." -Fore Red
+    }
+
+    Write-Host ""
+    Write-Host "compiling datasurf..." -Fore Cyan
+    Write-Host ""
+
+    if(-Not (Test-Path "./bin/$build/" -PathType Container))
+    {
+        mkdir "./bin/$build/"
+    }
+
+    Write-Host "compiling $build build with the following command:"
+    Write-Host "$script:compiler $1"
+    &$script:compiler @1
+    if($LASTEXITCODE -ne 0)
+    {
+        Write-Host "`nERROR: $script:compiler failed to compile datasurf.`n" -Fore Red
+        exit -1
+    }
+    Move-Item ./a.exe ./bin/$build/datasurftest.exe -Force
+    if($build -eq "release")
+    {
+        return;
+    }
+    Move-Item ./a.pdb ./bin/$build/datasurftest.pdb -Force
+}
+
+if($build -eq "release")
+{
+    compile ($args_always + $args_release)
+}
+elseif($build -eq "debug")
+{
+    compile ($args_always + $args_debug)
+}
+elseif($build -eq "asan")
+{
+    compile ($args_always + $args_asan)
 }
 else
 {
-     Write-Host "ERROR: invalid make config: '$build'." -ForegroundColor Red
-     exit -2;
-}
-
-if($LASTEXITCODE -ne 0)
-{
-     Write-Host "`nERROR: failed to compile imgsurf.`n" -ForegroundColor Red
-     exit -1;
+    Write-Host "`nERROR: invalid make config: $build." -Fore Red
+    exit 3;
 }
 
 Write-Host "`n"
 
 if($compile_only -eq "--compile-only")
 {
-    exit 0;
+    exit 0
 }
 
-&./bin/$build/imgsurftest.exe
+&./bin/$build/datasurftest
